47d544ea84ddb80dcb28969dc09d09e00f7c08b2
Dominic Scheurer <dom.scheurer@gmail.com>
2014-11-12 08:21:25 +0100

    FSM adjust alphabet functionality

--- src/de/dominicscheurer/fsautils/FSM.scala
+++ src/de/dominicscheurer/fsautils/FSM.scala
@@ -17,33 +17,33 @@
  */
 
 package de.dominicscheurer.fsautils {
-    
+
     import Types._
     import Conversions._
     import RegularExpressions._
-	
+
     abstract class FSM {
-       def isDFA = this.isInstanceOf[DFA]
-       def isNFA = this.isInstanceOf[NFA]
-       def asDFA: Option[DFA] =
-           if (isDFA)
-               Some(this.asInstanceOf[DFA])
-           else
-               None
-       def asNFA: Option[NFA] =
-           if (isNFA)
-               Some(this.asInstanceOf[NFA])
-           else
-               None
-       
-       def toXml: scala.xml.Elem
-               
-       def toPrettyXml: String = {
+        def isDFA = this.isInstanceOf[DFA]
+        def isNFA = this.isInstanceOf[NFA]
+        def asDFA: Option[DFA] =
+            if (isDFA)
+                Some(this.asInstanceOf[DFA])
+            else
+                None
+        def asNFA: Option[NFA] =
+            if (isNFA)
+                Some(this.asInstanceOf[NFA])
+            else
+                None
+
+        def toXml: scala.xml.Elem
+
+        def toPrettyXml: String = {
             val printer = new scala.xml.PrettyPrinter(80, 2)
             printer.format(toXml)
         }
-               
-       def toStringUpToDelta(
+
+        def toStringUpToDelta(
             indentBeginner: String,
             indentSpace: String,
             alphabetDesignator: String,
@@ -53,45 +53,51 @@ package de.dominicscheurer.fsautils {
             initialStateDesignator: String,
             initialState: State,
             acceptingDesignator: String,
-            accepting: Set[State]) : String = {
-              
+            accepting: Set[State]): String = {
+
             val indent = indentBeginner + indentSpace
             val dindent = indent + indentSpace
             var sb = new StringBuilder()
-                 
+
             sb ++= indent ++= alphabetDesignator ++= " = {"
             alphabet.foreach(s => sb ++= s.name ++= ",")
             sb = sb.dropRight(1 - alphabet.isEmpty)
             sb ++= "}\n"
-            
+
             sb ++= indent ++= statesDesignator ++= " = {"
             states.foreach(s => sb ++= s.toString() ++= ",")
             sb = sb.dropRight(1 - states.isEmpty)
             sb ++= "}\n"
-              
+
             sb ++= indent ++= initialStateDesignator ++= " = " ++= initialState.toString ++= "\n"
-            
+
             sb ++= indent ++= acceptingDesignator ++= " = {"
             accepting.foreach(s => sb ++= s.toString() ++= ",")
             sb = sb.dropRight(1 - accepting.isEmpty)
             sb ++= "}\n"
-              
+
             sb.toString
         }
-       
+
         def accepts(word: String): Boolean
 
         def accepts(word: Word): Boolean
 
         def accepts(word: Word, fromState: State): Boolean
+
+        def extendAlphabet(newLetters: Set[Letter]): NFA
         
-        def extendAlphabet(newLetters: Set[Letter]): FSM
+        def adjustAlphabet(other: FSM): NFA =
+            if (other.isDFA)
+                extendAlphabet(other.asDFA.get.alphabet)
+            else
+                extendAlphabet(other.asNFA.get.alphabet)
 
         def unary_! : FSM
 
         def * : FSM
 
-        def ++(other: DFA): FSM        
+        def ++(other: DFA): FSM
         def ++(other: NFA): FSM
         def ++(other: FSM): FSM =
             if (other isDFA)
